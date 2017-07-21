b6bc90f262b9ac585e36247af228943e353e7fc9
Grégoire Brechenmacher <greg.brech@hotmail.fr>
2017-07-21 12:00:56 +0200

    Amélioration du chat, is_typing en cours

--- nodeJS/chat.js
+++ nodeJS/chat.js
@@ -4,7 +4,7 @@ var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
 
-	res.sendFile( 'C:/wamp/www/test/chat.html');
+	res.sendFile( 'C:/wamp/www/gregoire/self_learning/nodeJS/chat.html');
 
 });
 
@@ -16,10 +16,17 @@ nsp.on( 'connection' , function( socket ) {
 	// ------- Message de connexion
 	nsp.emit( 'connection message' , 'A user just connected to nsp !' );
 
-	// ------- A la réception d'un message on l'envoi à tout le monde
-	socket.on('chat message', function( msg , nickname ){
+	// ------- Quand quelqu'un tape
+	socket.on( 'is_typing' , function( nickname ) {
 
-    	nsp.emit('chat message', nickname + ' : ' + msg);
+		socket.broadcast.emit( 'broadcast' , nickname + ' is typing...' );
+
+	});
+
+	// ------- A la réception d'un message on l'envoi à tout le monde
+	socket.on( 'chat message' , function( msg , nickname ){
+
+    	nsp.emit( 'chat message' , nickname + ' : ' + msg );
     	//socket.broadcast.emit( 'chat message' , nickname + ' : ' + msg );
 
   	});
